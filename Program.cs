e5ebdacccf6a27a500b3172b8837ddc3637e995c
AlfredoLG <dluxg1@miumg.edu.gt>
2025-08-02 22:20:59 -0600

    hl

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("holaaa este es mi cambio");
+Console.WriteLine("Hola, mundo!");
+Console.WriteLine("¡Hola, C#!");
